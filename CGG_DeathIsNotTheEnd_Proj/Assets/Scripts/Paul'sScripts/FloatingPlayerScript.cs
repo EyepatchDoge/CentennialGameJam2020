50654a01339319b06b9e54c20b4b3ffe54d1cc9e
Pauldrummondo <pauldrummond.biz@gmail.com>
2020-11-02 20:21:34 -0500

    more controllers
    added floatingPlayerScript for ghost

---
+++ CGG_DeathIsNotTheEnd_Proj/Assets/Scripts/Paul'sScripts/FloatingPlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingPlayerScript : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Vector2 MovementV;
+    public float speed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        MovementV = moveInput.normalized * speed;
+
+        //rb.MovePosition(rb.position + MovementV * Time.deltaTime);
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + MovementV * Time.fixedDeltaTime);
+    }
+}
