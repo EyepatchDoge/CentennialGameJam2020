28c46bf9336de2a7548203290b29959308369aad
Pauldrummondo <pauldrummond.biz@gmail.com>
2020-11-03 15:46:36 -0500

    progress
    added camfollow script to ghost and skeleton scenes, fully finished the layout of the skeleton scene(whiteboxed)

---
+++ CGG_DeathIsNotTheEnd_Proj/Assets/Scripts/Paul'sScripts/CamFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    public Vector3 camOffset;
+    public Transform Target;
+
+    public float SmoothOffset = 0.145f;
+
+    public void FixedUpdate()
+    {
+        Vector3 desirePose = Target.position + camOffset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, desirePose, SmoothOffset);
+        transform.position = smoothPosition;
+
+        transform.LookAt(Target);
+    }
+}
