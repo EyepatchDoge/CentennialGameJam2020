7f75d6bddd506b86a0460cf06c37cfbbd2b376d4
bracco99 <bracco99@hotmail.com>
2020-11-03 10:59:45 -0500

    Alex_Main Menu
    Alex Scene (Main Menu with Settings)

---
+++ CGG_DeathIsNotTheEnd_Proj/Assets/Scripts/Alex's Scripts/SceneController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public GameObject SettingsScreen;
+    public void PlayGameButton()
+    {
+        SceneManager.LoadScene("PaulScene");
+    }
+
+    public void OptionsButton()
+    {
+        SettingsScreen.SetActive(true);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
