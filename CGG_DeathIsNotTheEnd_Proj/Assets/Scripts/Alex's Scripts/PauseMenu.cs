47fa42efb74f87ca640ff6bfbd0b4ccf1b4b200d
bracco99 <bracco99@hotmail.com>
2020-11-03 12:32:19 -0500

    Pause and Settings Menus
    Pause and Settings are now prefabs and can be added anywhere.

---
+++ CGG_DeathIsNotTheEnd_Proj/Assets/Scripts/Alex's Scripts/PauseMenu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject SettingsScreen;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused && SettingsScreen.activeSelf == false)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        GameIsPaused = true;
+    }
+
+    public void SettingsMenuButton()
+    {
+        SettingsScreen.SetActive(true);
+    }
+
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene("Alex_Scene (Main Menu)");
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
