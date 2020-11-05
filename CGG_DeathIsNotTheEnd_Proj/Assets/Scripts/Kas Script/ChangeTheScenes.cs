11b388bc6b51b149c943fa9cc783dc82f8b01a0a
EyepatchDoge <kaspo.eyepatch@gmail.com>
2020-11-04 23:23:54 -0500

    Kas_Update19
    - Made a loop for first level
    - added scenes to build settings
    - added music to level 1
    - added a next level panel

---
+++ CGG_DeathIsNotTheEnd_Proj/Assets/Scripts/Kas Script/ChangeTheScenes.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeTheScenes : MonoBehaviour
+{
+    public void NextScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitFromGame()
+    {
+        Application.Quit();
+        Debug.Log("The Game has quit");
+    }
+}
